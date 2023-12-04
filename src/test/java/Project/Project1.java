1989457cfc99f930fec9a093a360dd0b3265f9ac
PranavSingh2410 <pranavks232@gmail.com>
2023-12-04 19:09:45 +0530

    first commiy

---
+++ src/test/java/Project/Project1.java
@@ -0,0 +1,13 @@
+package Project;
+
+import org.testng.annotations.Test;
+
+public class Project1 {
+
+	@Test
+	public void M2() 
+	{
+		System.out.println(" new project to push for maven");
+	}
+
+}
---
+++ src/test/java/Project/Project2.java
@@ -0,0 +1,13 @@
+package Project;
+
+import org.testng.annotations.Test;
+
+public class Project2 {
+
+	@Test
+	public void M1() 
+	{
+		System.out.println("new project for maven push");
+	}
+
+}
